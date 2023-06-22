46537486749ffb7c48a6cd4771f9029f46349c9e
김지원 <127467108+gamjawon@users.noreply.github.com>
2023-06-22 15:54:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const axios = require('axios')
 var cors = require('cors')
 const app = express()
 const port = 3000
@@ -15,13 +16,22 @@ app.get('/sound/:name', (req, res) => {
     if(name == 'dog') {
         res.json({'sound': '멍멍'})
     } else if(name == 'cat') {
-        res.json({'sound': '야옹'})
+        res.json({'sound': '야옹', catPicture})
     } else if(name == 'pig') {
         res.json({'sound': '꿀꿀'})
     } else {
         res.json({'sound': '알수없음'})
     }
 })
-
+async function catPicture() {
+    try {
+      const response = await axios.get('https://api.thecatapi.com/v1/images/search');
+      const catPictureUrl = response.data[0].url;
+      return catPictureUrl;
+    } catch (error) {
+      console.error('고양이 사진을 가져오는 동안 오류가 발생했습니다:', error);
+      return null;
+    }
+  }
 
 app.listen(3000)
